e09de0e9cfe6c44df4418884a6d661093cb28d66
Luis Mendes <machadoluismendes@gmail.com>
2021-08-31 09:25:06 -0300

    pip install anyio

---
+++ test/AsyncBackendTest.py
@@ -0,0 +1,15 @@
+# https://www.starlette.io/testclient/#testing-websocket-sessions
+from starlette.responses import HTMLResponse
+from starlette.testclient import TestClient
+
+
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+    response = HTMLResponse('<html><body>Hello, world!</body></html>')
+    await response(scope, receive, send)
+
+
+def test_app():
+    client = TestClient(app)
+    response = client.get('/')
+    assert response.status_code == 200
