f2d0e016c11901ca8af33c4aed761f0210166ffb
Luis Mendes <machadoluismendes@gmail.com>
2021-08-31 12:00:03 -0300

    asgi server

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+# from starlette.applications import Starlette
 from starlette.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 import uvicorn
 
+# asgi server netlify
+uvicorn.run(..., http='h11', loop='asyncio')
+
 
 templates = Jinja2Templates(directory='templates')
 
-app = Starlette(debug=True)
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+])
 app.mount('/static', StaticFiles(directory='statics'), name='static')
 
 
